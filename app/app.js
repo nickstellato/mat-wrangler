3e21e68d881535ec82cdd2931fc6ab2ab21d12cf
Nick Stellato <nick@nickstellato.com>
2018-02-18 16:01:02 -0600

    you can no reach a specific mat by including the array based id

--- app/app.js
+++ app/app.js
@@ -5,6 +5,13 @@ var dataFile = require('./data/mats.json');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
+	res.send(`
+		<h1>Welcome</h1>
+		<p>Mat Wrangler is a web application for manging your bullpen at your wrestling tournament.</p>
+	`);
+});
+
+app.get('/mats', function(req, res) {
 	var mats = '';
 	dataFile.mats.forEach(function(mat){
 		mats += `<h2>${mat.name}</h2>`;
@@ -12,6 +19,13 @@ app.get('/', function(req, res) {
 	res.send(`
 		<h1>Mat Wrangler</h1>
 		${mats}
+	`);
+});
+
+app.get('/mats/:id', function(req, res) {
+	var mat = dataFile.mats[req.params.id];
+	res.send(`
+		<h1>${mat.name}</h1>
 	`);
 });
 
