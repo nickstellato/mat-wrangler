ae729f070f491e5f1c6588d5e34df7f714b075ed
Nick Stellato <nick@nickstellato.com>
2018-02-18 15:40:13 -0600

    Updated port to use environment instead of hardcoding port 3000.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var dataFile = require('./data/mats.json');
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
 	res.send('<h1>Mat Wrangler</h1>');
 });
 
-var server = app.listen(3000, function() {
-	console.log('Listening on port 3000');
+var server = app.listen(app.get('port'), function() {
+	console.log('Listening on port ' + app.get('port'));
 });
