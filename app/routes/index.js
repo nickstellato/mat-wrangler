44a538736c364db59eea3eefedf60f1af4754a6d
Nick Stellato <nick@nickstellato.com>
2018-02-18 23:00:16 -0600

    using separate index.js routes folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/', function(req, res) {
-	res.send(`
-		<h1>Welcome</h1>
-		<p>Mat Wrangler is a web application for manging your bullpen at your wrestling tournament.</p>
-		<script src="/reload/reload.js"></script>
-	`);
+router.get('/', function (req, res) { 
+	res.render('index', {
+		pageTitle: 'Home', 
+		pageID: 'home'
+	});
 });
 
 module.exports = router;
