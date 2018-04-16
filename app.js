6c8b25e73e0817287890e3564097ed3031ac43dd
Nick Stellato <nick@nickstellato.com>
2018-04-15 20:36:47 -0500

    started to make some facia changes

--- app.js
+++ app.js
@@ -48,10 +48,6 @@ app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
 });
 
-// app.get('*', (req, res) => {
-//   res.send('/public/index.html');
-// });
-
 // Start Server
 app.listen(port, () => {
     console.log('Server started on port ' + port);
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://heroku_64mzjtp2:frq47h5i4crs7akt97lbq3fcrt@ds143221.mlab.com:43221/heroku_64mzjtp2',
+    database: 'mongodb://localhost:27017/',
     secret: 'yoursecret'
 }
